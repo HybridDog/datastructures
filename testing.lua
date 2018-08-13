6fe8745e2ffdb74354bb3e3de3d894cec1984e1d
HybridDog <ovvv@web.de>
2018-08-13 11:46:37 +0200

    Add pairing heap

--- testing.lua
+++ testing.lua
@@ -6,7 +6,7 @@ stack:push"snd"
 print(stack:to_string())
 print(stack:pop(), stack:pop(), stack:is_empty(), stack:size())
 print(stack:to_string())
-print"\n"
+print""
 
 print"testing fifo"
 local fifo = datastructures.create_fifo()
@@ -16,5 +16,19 @@ fifo:add"snd"
 print(fifo:to_string())
 print(fifo:take(), fifo:take(), fifo:is_empty(), fifo:size())
 print(fifo:to_string())
-print"\n"
+print""
+
+print"testing pairing heap"
+local heap = datastructures.create_pairing_heap(function(a, b)
+	return b - a
+end)
+print(heap:to_string())
+heap:add(1)
+heap:add(6)
+heap:add(3)
+heap:add(-5)
+print(heap:to_string())
+print(heap:take(), heap:take(), heap:is_empty(), heap:size())
+print(heap:to_string())
+print""
 
